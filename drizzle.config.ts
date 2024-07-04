9daf43d24cb459352813b96f868794e1dc95d057
jonz94 <jody16888@gmail.com>
2024-07-04 15:23:53 +0800

    feat: initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default defineConfig({
+  schema: "./src/server/db/schema.ts",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["kuromu-t3_*"],
+});
