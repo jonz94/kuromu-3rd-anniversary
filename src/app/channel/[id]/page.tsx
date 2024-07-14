3defb7bfbb07af05ca4fd0d923a6f5317c1fe861
jonz94 <jody16888@gmail.com>
2024-07-15 00:50:15 +0800

    refactor: minor refactor

--- src/app/channel/[id]/page.tsx
+++ src/app/channel/[id]/page.tsx
@@ -5,6 +5,8 @@ import { ModeToggle } from '~/components/ModeToggle'
 import { NavigateBack } from './NavigateBack'
 
 export default function Page({ params }: { params: { id: string } }) {
+  const channelId = params.id
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-12 px-4 pb-16 pt-24 sm:pt-16">
       <div className="absolute left-8 top-8">
@@ -16,13 +18,13 @@ export default function Page({ params }: { params: { id: string } }) {
       </div>
 
       <div className="min-h-16">
-        <Channel channelId={params.id} />
+        <Channel channelId={channelId} />
       </div>
 
       <LastUpdatedAt />
 
-      <div className="flex w-full max-w-screen-xl grow flex-col">
-        <MessageList channelId={params.id} />
+      <div className="flex w-full grow flex-col">
+        <MessageList channelId={channelId} />
       </div>
     </main>
   )
