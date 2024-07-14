2c1629d15312c7b2a4cc635d55bae77f687c4f14
jonz94 <jody16888@gmail.com>
2024-07-14 20:11:01 +0800

    feat: add navigate back button

--- src/app/channel/[id]/page.tsx
+++ src/app/channel/[id]/page.tsx
@@ -1,11 +1,22 @@
+import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
 import { Channel } from '~/app/_components/Channel'
 import { LastUpdatedAt } from '~/app/_components/LastUpdatedAt'
 import { MessageList } from '~/app/_components/MessageList'
 import { ModeToggle } from '~/components/ModeToggle'
+import { Button } from '~/components/ui/button'
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="absolute top-8 left-8">
+        <Button asChild size="icon">
+          <Link href="/" prefetch={false}>
+            <ChevronLeft className="size-4" />
+          </Link>
+        </Button>
+      </div>
+
       <div className="absolute top-8 right-8">
         <ModeToggle></ModeToggle>
       </div>
