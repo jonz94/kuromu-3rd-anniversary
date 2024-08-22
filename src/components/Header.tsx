efc08c33c8163435e832b8575ba430851bebcd04
jonz94 <jody16888@gmail.com>
2024-08-23 04:56:53 +0800

    fix: resolve "header is under the content" issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export function Header() {
   const pathname = usePathname()
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:px-6">
+    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:px-6">
       <nav className="hidden w-full flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
           <Gift className="size-6" />
